91a4e49cb0ceddf95994ab76a1e02799ad17be65
jongjin_na <njjwa1004@naver.com>
2021-10-08 22:30:48 +0900

    :memo: Fr_Heston FILE

---
+++ Project/HestonModel/FR_Heston.py
@@ -0,0 +1,107 @@
+# http://gouthamanbalaraman.com/blog/heston-calibration-scipy-optimize-quantlib-python.html
+
+import QuantLib as ql
+from math import pow, sqrt
+import numpy as np
+from scipy.optimize import least_squares
+
+day_count = ql.Actual365Fixed()
+calendar = ql.UnitedStates()
+calculation_date = ql.Date(8, 10, 2021)
+
+spot = 659.37
+ql.Settings.instance().evaluationDate = calculation_date
+
+risk_free_rate = 0.01
+dividend_rate = 0.0
+yield_ts = ql.YieldTermStructureHandle(
+    ql.FlatForward(calculation_date, risk_free_rate, day_count))
+dividend_ts = ql.YieldTermStructureHandle(
+    ql.FlatForward(calculation_date, dividend_rate, day_count))
+
+expiration_dates = [ql.Date(30,10,2021), ql.Date(30,11,2021), ql.Date(30,12,2021)]
+strikes = [74500, 74750, 75000, 75250, 75500, 75750, 76000]
+data = [
+[10, 5, 5, 5, 5, 5, 5],
+[220, 130, 80, 50, 30, 20, 15],
+[320, 270, 160, 120, 100, 80, 40]]
+
+def setup_helpers(engine, expiration_dates, strikes, 
+                  data, ref_date, spot, yield_ts, 
+                  dividend_ts):
+    heston_helpers = []
+    grid_data = []
+    for i, date in enumerate(expiration_dates):
+        for j, s in enumerate(strikes):
+            t = (date - ref_date )
+            p = ql.Period(t, ql.Days)
+            vols = data[i][j]
+            helper = ql.HestonModelHelper(
+                p, calendar, spot, s, 
+                ql.QuoteHandle(ql.SimpleQuote(vols)),
+                yield_ts, dividend_ts)
+            helper.setPricingEngine(engine)
+            heston_helpers.append(helper)
+            grid_data.append((date, s))
+    return heston_helpers, grid_data
+
+def cost_function_generator(model, helpers,norm=False):
+    def cost_function(params):
+        params_ = ql.Array(list(params))
+        model.setParams(params_)
+        error = [h.calibrationError() for h in helpers]
+        if norm:
+            return np.sqrt(np.sum(np.abs(error)))
+        else:
+            return error
+    return cost_function
+
+def calibration_report(helpers, grid_data, detailed=False):
+    avg = 0.0
+    if detailed:
+        print("%15s %25s %15s %15s %20s" % (
+            "Strikes", "Expiry", "Market Value", 
+             "Model Value", "Relative Error (%)"))
+        print("="*100)
+    for i, opt in enumerate(helpers):
+        err = (opt.modelValue()/opt.marketValue() - 1.0)
+        date,strike = grid_data[i]
+        if detailed:
+            print("%15.2f %25s %14.5f %15.5f %20.7f " % (
+                strike, str(date), opt.marketValue(), 
+                opt.modelValue(), 
+                100.0*(opt.modelValue()/opt.marketValue() - 1.0)))
+        avg += abs(err)
+    avg = avg*100.0/len(helpers)
+    if detailed:
+      print("-"*100)
+    print("Average Abs Error (%%) : %5.9f" % (avg))
+    return avg
+
+def setup_model(_yield_ts, _dividend_ts, _spot, 
+                init_condition=(0.02,0.2,0.5,0.1,0.01)):
+    theta, kappa, sigma, rho, v0 = init_condition
+    process = ql.HestonProcess(_yield_ts, _dividend_ts, 
+                           ql.QuoteHandle(ql.SimpleQuote(_spot)), 
+                           v0, kappa, theta, sigma, rho)
+    model = ql.HestonModel(process)
+    engine = ql.AnalyticHestonEngine(model) 
+    return model, engine
+
+
+
+model3, engine3 = setup_model(
+    yield_ts, dividend_ts, spot, 
+    init_condition=(0.02,0.2,0.5,0.1,0.01))
+heston_helpers3, grid_data3 = setup_helpers(
+    engine3, expiration_dates, strikes, data,
+    calculation_date, spot, yield_ts, dividend_ts
+)
+initial_condition = list(model3.params())
+
+cost_function = cost_function_generator(model3, heston_helpers3)
+sol = least_squares(cost_function, initial_condition)
+theta, kappa, sigma, rho, v0 = model3.params()
+print("theta = %f, kappa = %f, sigma = %f, rho = %f, v0 = %f" % \
+    (theta, kappa, sigma, rho, v0))
+error = calibration_report(heston_helpers3, grid_data3, detailed=0)
