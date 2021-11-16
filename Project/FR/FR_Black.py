9315664f1a2b3f012cd8cebcc0cafe1191e19fd9
jongjin_na <njjwa1004@naver.com>
2021-11-16 20:10:32 +0900

    :memo: KW file

--- Project/FR/FR_Black.py
+++ Project/FR/FR_Black.py
@@ -1,14 +1,24 @@
 import QuantLib as ql
+import pandas as pd
+import numpy as np
+import csv
 
-valuationDate = ql.Date(12, 11, 2021)
+
+Data = pd.read_csv("Project\FR\OpFuData.csv", header=0, index_col="date")
+Data.index = pd.to_datetime(Data.index, format="%Y/%m/%d") # dtype = 'datetime64[ns]'로 나오고 1926-07-01처럼 일도 나옴
+Data.index = Data.index.to_period("d") # dtype='period[M]'으로 나오고 1926-07까지 나옴 / to_period를 쓰기 위해서 to_datetime으로 바꾼 뒤 사용
+print(Data)
+
+
+valuationDate = ql.Date(3, 11, 2021) # 바꿔야돼!
 expiryDate = ql.Date(3, 12, 2021)
-strikePrice = 1.11100
-underlying_qt = ql.SimpleQuote(1.14535)
-foreignrate_qt = ql.SimpleQuote(0.072)
-riskfreerate_qt = ql.SimpleQuote(0.0583)
-volatility_qt = ql.SimpleQuote(0.0)
-mkt_price = 0.00025
-option_type = ql.Option.Put
+strikePrice = 1.1650
+underlying_qt = ql.SimpleQuote(1.16200) # 바꿔야돼!
+foreignrate_qt = ql.SimpleQuote(-0.006659) 
+riskfreerate_qt = ql.SimpleQuote(0.00046)
+volatility_qt = ql.SimpleQuote(0.0461) 
+mkt_price = 0.005 # 바꿔야돼!
+option_type = ql.Option.Call # 바꿔야돼!
 
 ql.Settings.instance().evaluationDate = valuationDate
 calendar = ql.UnitedStates()
@@ -35,8 +45,9 @@ option = ql.VanillaOption(payoff, exercise)
 option.setPricingEngine(engine)
 
 # implied volatility
-implied_volaitility = option.impliedVolatility(mkt_price, process)
-volatility_qt.setValue(implied_volaitility)
+# implied_volaitility = option.impliedVolatility(mkt_price, process)
+# volatility_qt.setValue(implied_volaitility)
+# print(implied_volaitility)
 print('Option Premium = ', option.NPV())
 print('Option Delta = ', option.delta())
 print('Option Gamma = ', option.gamma())
